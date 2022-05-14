256ad1215d15062b479312bcb2ee564f8e43a081
MAURIGON <mauriciogzb1322@gmail.com>
2022-05-14 12:00:12 -0400

    Login

---
+++ Assets/Scripts/Currency.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ServerModels;
+
+
+public class VirtualCurrency : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddCurrency(10);//pasarPuntaje
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void AddCurrency(int amount)
+    {
+        AddUserVirtualCurrencyRequest request = new AddUserVirtualCurrencyRequest()
+        {
+            Amount =amount,
+            VirtualCurrency="CN",
+            PlayFabId=PlayerPrefs.GetString("PlayerFabId")
+        };
+      PlayFabServerAPI.AddUserVirtualCurrency(request, ResultCallback,ErrorCallback);
+    }
+    
+    private void ErrorCallback(PlayFabError obj){
+        Debug.Log(obj.ErrorMessage);
+    }
+    private void ResultCallback(ModifyUserVirtualCurrencyResult obj){
+        Debug.Log("Virtual Currency");
+        
+    }
+    public void ReduceVirtualCurrency()
+    {
+        SubtractUserVirtualCurrencyRequest request=new SubtractUserVirtualCurrencyRequest()
+        {
+            Amount =10,
+            VirtualCurrency="BS",
+            PlayFabId=PlayerPrefs.GetString("PlayerFabId")//PlayerPrefs.GetString("PlayFabId");
+        };
+        PlayFabServerAPI.SubtractUserVirtualCurrency(request, ResultCallbackVirtual,ErrorCallback);
+    }
+    
+     private void ResultCallbackVirtual(ModifyUserVirtualCurrencyResult obj){
+        Debug.Log("CNdescontado, CN actuales"+obj.Balance);
+        
+    }
+}
---
+++ Assets/Scripts/PlayerLogin.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerLogin : MonoBehaviour
+{
+    [SerializeField] private InputField usernameInputField;
+    [SerializeField] private InputField passwordInputField;
+    
+    public void OnLogin()
+    {
+        LoginWithPlayFabRequest request=new LoginWithPlayFabRequest()
+        {
+            Username=usernameInputField.text.Replace(" "," "),
+            Password=passwordInputField.text,
+            TitleId=PlayFabSettings.TitleId
+        };
+        PlayFabClientAPI.LoginWithPlayFab(request, OnResultCallback,ErrorCallBack);       
+
+    }
+    private void ErrorCallBack(PlayFabError error)
+        {
+            Debug.LogError("Here's some debug information:");
+            Debug.LogError(error.GenerateErrorReport());
+            string message = "";
+            if (error.ErrorDetails != null)
+ {
+    foreach (var i in error.ErrorDetails)
+    {
+        foreach (var j in i.Value)
+        {
+            if (j.Contains("Username contains invalid characters"))
+            {
+                message = message + "El Usuario contiene caracteres inválidos" + "\n";
+            }
+            else if (j.Contains("Username must be between 3 and 20 characters"))
+            {
+                message = message + "El Usuario debe tener entre 3 y 20 caracteres" + "\n";
+            }
+            else if (j.Contains("User name already exists"))
+            {
+                message = message + "El Usuario no está disponible" + "\n";
+            }
+            else if (j.Contains("Email address is not valid"))
+            {
+                message = message + "El Correo no es válido" + "\n";
+            }
+            else if (j.Contains("Email address already exists"))
+            {
+                message = message + "El Correo no está disponible" + "\n";
+            }
+            else if (j.Contains("Password must be between 6 and 100 characters"))
+            {
+                message = message + "La contraseña debe tener entre 6 y 100 caracteres" + "\n";
+            }
+            else if (j.Contains("User not found"))
+            {
+                message = message + "Usuario no encontrado" + "\n";
+            }
+            Debug.Log("********" + j);
+        }
+    }
+ }
+            if (message == "")
+             {
+               message = "Verifica que todos tus datos esten debidamente llenados e intenta de nuevo";
+             }
+
+             //NotificationPanel.Instance.ShowNotification(message);
+            
+            
+        }
+        private void OnResultCallback(LoginResult obj)
+        {
+            PlayerPrefs.SetString("PlayerFabId",obj.PlayFabId);
+            SceneManager.LoadScene(1);
+            Debug.Log("Inicio de sesion correcto");
+        }
+}
